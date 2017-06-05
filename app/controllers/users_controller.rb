ad88a0f0ec25f77ccbdb91800f2b499c47e96c98
Colleen <colleen.schnettler@gmail.com>
2017-06-05 10:27:45 -0400

    In line with tutorial up to (but not including) section 7.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new #Signup page is routed to "new" action.  Data will come from form 
   end
+
+  def show
+  	@user= User.find(params[:id]) #Show current user
+
+  end
+
+
 end
