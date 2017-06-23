74614d2d320d3350b0d03006e84e4134606cb8b0
Colleen <colleen.schnettler@gmail.com>
2017-06-23 11:11:17 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
   def create
   	@user=User.new(user_params)
   	  if @user.save
-  	  	  #render show 
-  	  	  #User save pop-up
-  	  else 
-  	  	render 'new'
+        flash[:success] = "Welcome to the Sample App"
+        redirect_to @user 
+      else 
+        render 'new'
   	  end
   	end
 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,12 +9,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	get signup_path 
     assert_select 'form[action="/signup"]'
   	assert_no_difference 'User.count' do
-  		post signup_path, params: {user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}}
+  		post signup_path, params: {user: {name: "", email: "user@invalid", password: "foo12345", password_confirmation: "bar"}}
   	end
   	assert_template 'users/new'
   	assert_select 'div.alert.alert-danger' #Test for main alert errors
   	assert_select 'div#error_explanation'
+  end
 
-
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count',1 do
+      post signup_path, params: {user: {name:"Test Name", email:"email@gmail.com", password:"foo12345", password_confirmation: "foo12345"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
